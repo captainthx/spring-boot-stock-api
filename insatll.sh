5179633af12bdab9f9a34efc69fa241767c02ecf
capptt <106069231+captainthx@users.noreply.github.com>
2023-11-08 16:46:11 +0700

    update script

--- insatll.sh
+++ insatll.sh
@@ -2,7 +2,7 @@ mvn clean install
 
 if [ $? -eq 0 ];then
   docker build -t server .
-  docker run --name server-container -d -p 8080:8080 server
+  docker start --name server-container -d -p 8080:8080 server
   docker ps
 else
   echo "Build failed"
--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ docker stop server-container
 docker rm server-container
 if [ $? -eq ]; then
   docker build -t server .
-  docker run --name server-container -d -p 8080:8080 server
+  docker start --name server-container -d -p 8080:8080 server
   docker ps
 else
   echo "Build failed"
