2ca86b9e96c04c53c1ad7062c742c954613d5d27
capptt <106069231+captainthx@users.noreply.github.com>
2023-11-08 17:20:11 +0700

    update script

--- insatll.sh
+++
@@ -1,9 +0,0 @@
-mvn clean install
-
-if [ $? -eq 0 ];then
-  docker build -t server .
-  docker start --name server-container -d -p 8080:8080 server
-  docker ps
-else
-  echo "Build failed"
-fi
---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#/ network_name=""
+container_name="server-container"
+image_name="server"
+
+if docker ps | grep -q "$container_name"; then
+    echo "Container "$container_name" already exists. Stopping and removing it."
+    docker stop $container_name
+    docker rm $container_name
+fi
+
+# Build the Maven project
+mvn clean install
+
+# Check if the Maven build was successful or if there were any errors
+if [ $? -eq 0 ]; then
+  echo "Maven build succeeded."
+  # Build a Docker image
+  docker build -t $image_name .
+
+  # Run a Docker container
+  docker run --name $container_name -v ${PWD}/logs:/logs -p 8080:8080 -d $image_name
+
+  # Display a list of running Docker containers
+  docker ps
+else
+  echo "Maven build failed."
+fi
